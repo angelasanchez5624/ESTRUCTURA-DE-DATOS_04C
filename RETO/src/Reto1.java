f4ba185694db4194c86805316a47ead6c503c262
angelasanchez5624 <angelasanchez5624@gmail.com>
2026-02-27 20:44:47 -0500

    Ejercicios

--- RETO/src/Reto1.java
+++ RETO/src/Reto1.java
@@ -1,3 +1,23 @@
+// Reto 1: El Cajero del Supermercado (Vectores y Sumatorias)
+
+// Objetivo: Llenar un vector manualmente y procesar la información.
+
+// El Problema: El usuario debe ingresar los precios de 5 productos que compró en el mercado para calcular el total.
+
+// Instrucciones:
+
+// Crea un vector double[] precios = new double[5];
+
+// Usa un ciclo for y Scanner para pedirle al usuario que ingrese los 5 precios uno por uno.
+
+// Tarea A: Calcula la suma total de los precios ingresados.
+
+// Tarea B: Encuentra cuál fue el precio más alto que se ingresó y muéstralo.
+
+// Tarea C: Si el total supera los 20.000, muestra un mensaje:
+// "¡Tienes un descuento de 2.000!".
+// Resta ese valor y muestra el total final a pagar.
+
 import java.util.Scanner;
 
 public class Reto1 {
